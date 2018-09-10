378efd749882cf5f2759605553e8c200c1942995
Andrew Bernier <a6bernier@gmail.com>
2018-09-10 18:35:31 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,4 +1,12 @@
 def my_select(collection)
-  collection.select |x|
+  i = 0
+  true_collection = []
+
+  while i < collection.length
+    if yield(collection[i])
+      true_collection << collection[i]
+    end
+    i += 1
   end
+  true_collection
 end
